2ba5fe2f76df01adac7c744c468b2b1470aa3104
lantian <lantian@atmire.com>
2014-07-24 16:35:22 -0700

    Fix load credit

--- dspace/modules/api/src/main/java/org/dspace/app/util/LoadCustomerCredit.java
+++ dspace/modules/api/src/main/java/org/dspace/app/util/LoadCustomerCredit.java
@@ -4,16 +4,20 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.PosixParser;
-import org.apache.commons.jxpath.xml.DOMParser;
+
+import org.apache.commons.httpclient.HttpClient;
+
+
+import org.apache.commons.httpclient.methods.GetMethod;
+
 import org.apache.log4j.Logger;
+
 import org.apache.xml.serialize.OutputFormat;
 import org.apache.xml.serialize.XMLSerializer;
-import org.dspace.content.DCDate;
-import org.dspace.content.Item;
+
 import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Context;
 
-import javax.net.ssl.HttpsURLConnection;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
@@ -21,18 +25,19 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+
 
 import org.w3c.dom.*;
 import org.xml.sax.InputSource;
+
+
+
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+
+
+
 
 /**
  * User: lantian @ atmire . com
@@ -98,104 +103,67 @@ public class LoadCustomerCredit {
                 String requestUrl = ConfigurationManager.getProperty("association.anywhere.link");
 
                 try {
-                    String url = requestUrl;
-                    URL obj = new URL(url);
-                    URLConnection con = obj.openConnection();
 
-                    String userName = ConfigurationManager.getProperty("association.anywhere.username");
-                    String passWord = ConfigurationManager.getProperty("association.anywhere.password");
+                    String url = requestUrl.replace("customerId",customerId);
+                    HttpClient client = new HttpClient();
 
-//                    <?xml version="1.0" encoding="UTF-8" ?>
-//                    <creditRequest>
-//                    <integratorUsername>datadryad<integratorUsername>
-//                    <integratorPassword>D8ta1ntgr8tn</integratorPassword>
-//                    <custId>1</custId>
-//                    <txTy>PREPAID</txTy>
-//                    </creditRequest>
+                    GetMethod get = new GetMethod(url);
 
-                    String paiedType = "paiedType";
+//                    HttpMethodParams params = new HttpMethodParams();
+//
+//                    params.setParameter("test","test");
+//
+//                    get.setParams(params);
 
-                    DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-                    DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+                    client.executeMethod(get);
 
-                    // root elements
-                    Document doc = docBuilder.newDocument();
-                    doc.setXmlVersion("1.0");
-
-                    Element rootElement = doc.createElement("creditRequest");
-                    doc.appendChild(rootElement);
-
-                    Element user = doc.createElement("integratorUsername");
-                    user.appendChild(doc.createTextNode(userName));
-                    rootElement.appendChild(user);
-
-                    Element pass = doc.createElement("integratorPassword");
-                    pass.appendChild(doc.createTextNode(passWord));
-                    rootElement.appendChild(pass);
-
-                    Element customer = doc.createElement("custId");
-                    customer.appendChild(doc.createTextNode(customerId));
-                    rootElement.appendChild(customer);
-
-                    Element txTy = doc.createElement("txTy");
-                    txTy.appendChild(doc.createTextNode(paiedType));
-                    rootElement.appendChild(txTy);
-
-                    OutputFormat format    = new OutputFormat (doc);
-                    // as a String
-                    StringWriter stringOut = new StringWriter ();
-                    XMLSerializer serial   = new XMLSerializer (stringOut,
-                            format);
-                    serial.serialize(doc);
-                    // Display the XML
-
-
-                    String urlParameters ="p_input_xml_doc="+stringOut.toString().replaceAll("\n|\r", "");
-                    // Send post request
-                    con.setDoOutput(true);
-                    DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-                    wr.writeBytes(urlParameters);
-                    wr.flush();
-                    wr.close();
-
-                    System.out.println("\nSending 'POST' request to URL : " + url);
-                    System.out.println("Post parameters : " + urlParameters);
-
-
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(con.getInputStream()));
-                    String inputLine;
-                    StringBuffer response = new StringBuffer();
-
-                    while ((inputLine = in.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-                    in.close();
-
-                    InputSource is = new InputSource(new StringReader(response.toString()));
-                    Document docResponse = docBuilder.parse(is);
-                    NodeList errors =  docResponse.getElementsByTagName("errors");
-                    if(errors!=null)
+                    if( get.getStatusCode() <= 299 )
                     {
-                       System.out.println("errors when updateing customer credit:"+response.toString());
-                       log.error("errors when updateing customer credit:"+response.toString());
-                    }
-                    else{
-                        NodeList list = docResponse.getElementsByTagName("client");
-                        if(list!=null&&list.item(0)!=null)
-                        {
-                            credit = list.item(0).getTextContent();
+                        System.out.println("Response Code : "
+                                + get.getStatusLine().getStatusCode());
+                        String result = get.getResponseBodyAsString();
+                        System.out.println("Response body : "+result);
+
+                        String[] result1 = result.split("totCreditsAccepted");
+                        if(result1!=null&&result1.length>3){
+                            credit = result1[1];
                         }
+                        else
+                        {
+                            log.error("error when loading customer credit");
+                        }
+//
+//                        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+//                        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+//                        InputSource is = new InputSource(get.getResponseBodyAsStream());
+//                        Document docResponse = docBuilder.parse(is);
+//                        NodeList errors=  docResponse.getElementsByTagName("errors");
+//                        if(errors!=null)
+//                        {
+//                            int i = 0;
+//                            while(i < errors.getLength())
+//                            {
+//                                Node error = errors.item(i);
+//                                String value = error.getNodeValue();
+//                                log.error("error in loading client credit : "+ value);
+//                                i++;
+//                            }
+//                            log.error("errors when loading customer credit:"+result);
+//                        }
+//                        else{
+//                            NodeList list = docResponse.getElementsByTagName("totCreditsAccepted");
+//                            if(list!=null&&list.item(0)!=null)
+//                            {
+//                                credit = list.item(0).getTextContent();
+//                            }
+//                        }
+
                     }
 
 
-
-
-//            if(ConfigurationManager.getProperty("payment-system","paypal.returnurl").length()>0)
-//            get.addParameter("RETURNURL", ConfigurationManager.getProperty("payment-system","paypal.returnurl"));
                 }
                 catch (Exception e) {
-                    log.error("errors when updateing customer credit:", e);
+                    log.error("errors when loading customer credit:", e);
                     return null;
                 }
 
@@ -216,125 +184,44 @@ public class LoadCustomerCredit {
             // circumvent authorisation
 
             String credit=null;
-            try {
 
-
-                String requestUrl = ConfigurationManager.getProperty("association.anywhere.link");
+                String requestUrl = ConfigurationManager.getProperty("association.anywhere.updatelink");
 
                 try {
-                    String url = requestUrl;
-                    URL obj = new URL(url);
-                    URLConnection con = obj.openConnection();
+                        String url = requestUrl.replace("customerId",customerId);
+                        HttpClient client = new HttpClient();
 
-                    String userName = ConfigurationManager.getProperty("association.anywhere.username");
-                    String passWord = ConfigurationManager.getProperty("association.anywhere.password");
+                        GetMethod get = new GetMethod(url);
 
-    //                    <?xml version="1.0" encoding="UTF-8" ?>
-    //                    <creditRequest>
-    //                    <integratorUsername>datadryad<integratorUsername>
-    //                    <integratorPassword>D8ta1ntgr8tn</integratorPassword>
-    //                    <custId>1</custId>
-    //                    <txTy>PREPAID</txTy>
-    //                    </creditRequest>
+//                    HttpMethodParams params = new HttpMethodParams();
+//
+//                    params.setParameter("test","test");
+//
+//                    get.setParams(params);
 
-                    String paiedType = "DEFERRED";
+                        client.executeMethod(get);
 
-                    DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-                    DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-
-                    // root elements
-                    Document doc = docBuilder.newDocument();
-                    doc.setXmlVersion("1.0");
-
-                    Element rootElement = doc.createElement("credit-request");
-                    doc.appendChild(rootElement);
-
-                    Element user = doc.createElement("vendor-id");
-                    user.appendChild(doc.createTextNode(userName));
-                    rootElement.appendChild(user);
-
-                    Element pass = doc.createElement("vendor-password");
-                    pass.appendChild(doc.createTextNode(passWord));
-                    rootElement.appendChild(pass);
-
-                    Element customer = doc.createElement("cust-id");
-                    customer.appendChild(doc.createTextNode(customerId));
-                    rootElement.appendChild(customer);
-
-                    Element txTy = doc.createElement("trans-type");
-                    txTy.appendChild(doc.createTextNode(paiedType));
-                    rootElement.appendChild(txTy);
-
-                    DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-                    java.util.Date date = new java.util.Date();
-                    Element dateElement = doc.createElement("trans-date");
-                    dateElement.appendChild(doc.createTextNode(dateFormat.format(date)));
-                    rootElement.appendChild(txTy);
-
-                    Element deduct = doc.createElement("cred-accepted");
-                    deduct.appendChild(doc.createTextNode("-1"));
-                    rootElement.appendChild(deduct);
-
-
-                    OutputFormat format    = new OutputFormat (doc);
-                    // as a String
-                    StringWriter stringOut = new StringWriter ();
-                    XMLSerializer serial   = new XMLSerializer (stringOut,
-                            format);
-                    serial.serialize(doc);
-                    // Display the XML
-
-
-                    String urlParameters ="p_input_xml_doc="+stringOut.toString().replaceAll("\n|\r", "");
-                    // Send post request
-                    con.setDoOutput(true);
-                    DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-                    wr.writeBytes(urlParameters);
-                    wr.flush();
-                    wr.close();
-
-                    System.out.println("\nSending 'POST' request to URL : " + url);
-                    System.out.println("Post parameters : " + urlParameters);
-
-
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(con.getInputStream()));
-                    String inputLine;
-                    StringBuffer response = new StringBuffer();
-
-                    while ((inputLine = in.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-                    in.close();
-
-                    InputSource is = new InputSource(new StringReader(response.toString()));
-                    Document docResponse = docBuilder.parse(is);
-                    NodeList errors =  docResponse.getElementsByTagName("errors");
-                    if(errors!=null)
-                    {
-                        System.out.println(response.toString());
-                        log.error("errors when updateing customer credit:"+response.toString());
-                    }
-                    else{
-                        NodeList list = docResponse.getElementsByTagName("client");
-                        if(list!=null&&list.item(0)!=null)
+                        if( get.getStatusCode() <= 299 )
                         {
-                            credit = list.item(0).getTextContent();
+                            System.out.println("Response Code : "
+                                    + get.getStatusLine().getStatusCode());
+                            String result = get.getResponseBodyAsString();
+                            System.out.println("Response body : "+result);
+
+                            int start = result.indexOf("totCreditsAccepted");
+                            int end = result.lastIndexOf("totCreditsAccepted") ;
+                            System.out.println("start : "+start+", end"+end);
+                            if(start>0&&end>0){
+                                credit = result.substring(start,end);
+                            }
+                            else
+                            {
+                                log.error("error when loading customer credit");
+                            }
                         }
                     }
 
-
-
-
-    //            if(ConfigurationManager.getProperty("payment-system","paypal.returnurl").length()>0)
-    //            get.addParameter("RETURNURL", ConfigurationManager.getProperty("payment-system","paypal.returnurl"));
-                }
-                catch (Exception e) {
-                    log.error("errors when updateing customer credit:", e);
-                    return null;
-                }
-
-            }catch (Exception e)
+            catch (Exception e)
             {
                 System.out.print(e);
                 System.out.print(e.getStackTrace());
